eddefc5acf5b38de58d97bfa99f77d2fbfbf9e94
ilios17 <ertzeff@yandex.ru>
2018-10-14 13:17:14 +0300

    валидация

--- js/map.js
+++ js/map.js
@@ -325,3 +325,36 @@ timeInSelect.addEventListener('change', function (evt) {
 timeOutSelect.addEventListener('change', function (evt) {
   changeTime(evt.target.selectedIndex, timeInSelect);
 });
+
+var roomsSelect = document.querySelector('#room_number');
+var guestsSelect = document.querySelector('#capacity');
+
+var setCustomRoomsValidity = function () {
+  roomsSelect.style.boxShadow = '0 0 1px 5px red';
+  if (roomsSelect.options[0].selected && !guestsSelect.options[2].selected) {
+    roomsSelect.setCustomValidity('Такое количество комнат подходит для 1 гостя');
+  } else if (roomsSelect.options[2].selected && guestsSelect.options[3].selected) {
+    roomsSelect.setCustomValidity('Такое количество комнат подходит для 1, 2 или 3 гостей');
+  } else if (roomsSelect.options[1].selected && (guestsSelect.options[0].selected || guestsSelect.options[3].selected)) {
+    roomsSelect.setCustomValidity('Такое количество комнат подходит для 1 или 2 гостей');
+  } else if (roomsSelect.options[3].selected && !guestsSelect.options[3].selected) {
+    roomsSelect.setCustomValidity('Такое количество комнат не подходит для гостей');
+  } else {
+    roomsSelect.setCustomValidity('');
+  }
+};
+
+var makeInvalidRed = function () {
+  var inputs = document.querySelectorAll('input');
+
+  for (var i = 0; i < inputs.length; i++) {
+    if (!inputs[i].validity.valid) {
+      inputs[i].style.boxShadow = '0 0 1px 5px red';
+    }
+  }
+};
+
+document.querySelector('.ad-form__submit').addEventListener('click', function () {
+  makeInvalidRed();
+  setCustomRoomsValidity();
+});
