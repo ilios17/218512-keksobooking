d4a3a20c0153e37ff784dbd5a82cb1dbc90a50a4
ilios17 <ertzeff@yandex.ru>
2018-11-03 12:19:36 +0300

    4-1

--- js/map.js
+++ js/map.js
@@ -153,47 +153,26 @@ var drawPins = function () {
   pinsList.appendChild(fragment);
 };
 
-var typeTranslator = function (type) {
-  var translatedType = '';
-  switch (type) {
-    case 'flat':
-      translatedType = 'Квартира';
-      break;
-    case 'bungalo':
-      translatedType = 'Бунгало';
-      break;
-    case 'house':
-      translatedType = 'Дом';
-      break;
-    case 'palace':
-      translatedType = 'Дворец';
-      break;
-  }
-  return translatedType;
+var typeTranslator = {
+  flat: 'Квартира',
+  bungalo: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
 };
 
+var getTitle = function (type) {
+  return typeTranslator[type];
+};
+
+
 var modifyFeaturesList = function (features, parent) {
-  var clone = parent.cloneNode(true);
   parent.innerHTML = '';
   for (var i = 0; i < features.length; i++) {
-    if (features[i] === 'wifi') {
-      parent.appendChild(clone.querySelector('.popup__feature--wifi'));
-    }
-    if (features[i] === 'dishwasher') {
-      parent.appendChild(clone.querySelector('.popup__feature--dishwasher'));
-    }
-    if (features[i] === 'parking') {
-      parent.appendChild(clone.querySelector('.popup__feature--parking'));
-    }
-    if (features[i] === 'washer') {
-      parent.appendChild(clone.querySelector('.popup__feature--washer'));
-    }
-    if (features[i] === 'elevator') {
-      parent.appendChild(clone.querySelector('.popup__feature--elevator'));
-    }
-    if (features[i] === 'conditioner') {
-      parent.appendChild(clone.querySelector('.popup__feature--conditioner'));
-    }
+    var featureName = features[i];
+    var feature = document.createElement('li');
+    feature.classList.add('popup__feature');
+    feature.classList.add('popup__feature--' + featureName);
+    parent.appendChild(feature);
   }
 };
 
@@ -212,7 +191,7 @@ var showAd = function (ad) {
   adCard.querySelector('.popup__title').innerHTML = ad.offer.title;
   adCard.querySelector('.popup__text--address').innerHTML = ad.offer.adress;
   adCard.querySelector('.popup__text--price').innerHTML = ad.offer.price + '₽/ночь';
-  adCard.querySelector('.popup__type').innerHTML = typeTranslator(ad.offer.type);
+  adCard.querySelector('.popup__type').innerHTML = getTitle(ad.offer.type);
   adCard.querySelector('.popup__text--capacity').innerHTML = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
   adCard.querySelector('.popup__text--time').innerHTML = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
   adCard.querySelector('.popup__photo').src = ad.offer.photos[0];
